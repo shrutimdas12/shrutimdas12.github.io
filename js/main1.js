9c039c063b02bdbd8596ef6f593f0b91aed33f7e
shrutimdas12 <152917622+shrutimdas12@users.noreply.github.com>
2023-12-05 04:11:46 -0500

    Update main1.js
    Previously added HTML file - now the correct file

--- js/main1.js
+++ js/main1.js
@@ -1,36 +1,177 @@
-<!DOCTYPE html>
-<html>
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import {OrbitControls} from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
+import {GLTFLoader} from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 
-<head>
-  <meta charset="utf-8" />
-  <title>A simple Three.JS App</title>
-  <link rel="stylesheet" href="css/styles.css" />
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Mansalva&family=Poppins&display=swap" rel="stylesheet">
-</head>
+const handUrl = new URL('../Models/Hand/ShortHandDifferentAnimations.glb', import.meta.url);
 
-<body>
-  <!-- <header>
-      <h1>Bharatanatyam Mudras</h1>
-    </header> -->
-  <div>
-    <p class="heading heading1">Bharatanatyam</p>
-    <p class="heading heading2">Mudras</p>
-  </div>
+// const handUrl = new URL("AlexCodeHand.glb", import.meta.url);
+// import modelUrl from './AlexCodeHand.glb';
 
+const renderer = new THREE.WebGLRenderer();
 
-  <main>
-    <div id="container3D"></div>
-  </main>
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-  <script type="module" src="js/main1.js"></script>
+document.body.appendChild(renderer.domElement);
 
-  <div>
-    <p class="note">Press ‘Right and Left Arrows’ on your Keypad to view individual Mudras animation!</p>
-    <p class="note">Press ‘Space’ on your Keypad to start the combined animation!</p>
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+scene.add(directionalLight);
+directionalLight.position.set(0, 5, 30);
+
+// const sLightHelper = new THREE.DirectionalLightHelper(directionalLight);
+// scene.add(sLightHelper);
+
+// directionalLight.castShadow = true;
+// directionalLight.shadow.camera.bottom = -12;
+
+// const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+// scene.add(dLightShadowHelper);
+
+const directionalLightBack = new THREE.DirectionalLight(0xFFFFFF, 1);
+scene.add(directionalLightBack);
+directionalLightBack.position.set(0, 5, -30);
+
+// const sLightHelperBack = new THREE.DirectionalLightHelper(directionalLightBack);
+// scene.add(sLightHelperBack);
+
+renderer.setClearColor(0x02063d);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0, 2, 15);
+orbit.update();
+
+// const grid = new THREE.GridHelper(30, 30);
+// scene.add(grid);
+
+const assetLoader = new GLTFLoader();
+
+let mixer;
+let action;
+let clipSet;
+let currentIndex = 0;
+assetLoader.load(handUrl.href, function(gltf) {
     
-  </div>
-</body>
+    const model = gltf.scene;
+    scene.add(model);
 
-</html>
+    console.log("i loaded", gltf);
+
+    // Play animation
+    mixer = new THREE.AnimationMixer(model);
+    const clips = gltf.animations;
+    
+
+    //Listen for Spacebar
+    document.addEventListener('keydown', (e) => {
+
+    // Check if spacebar pressed 
+    if(e.code === 'Space') { 
+
+        if (action) {
+            action.stop();
+        }
+
+    // Play a certain animation
+    const clip = THREE.AnimationClip.findByName(clips, 'Mudras');
+    action = mixer.clipAction(clip);
+    action.reset();
+    action.play();
+
+  
+    }
+  
+  });
+
+  clipSet = [];
+  clipSet[0] = THREE.AnimationClip.findByName(clips, 'Patakam'); 
+  clipSet[1] = THREE.AnimationClip.findByName(clips, 'Tripatakam');
+  clipSet[2] = THREE.AnimationClip.findByName(clips, 'Ardhapatakam');
+  clipSet[3] = THREE.AnimationClip.findByName(clips, 'Aralam');
+  clipSet[4] = THREE.AnimationClip.findByName(clips, 'Shukhathundam');
+
+  console.log(clipSet);
+
+  //Listen for Left and Right Arrows
+  document.addEventListener('keydown', (e) => {
+
+
+    if (e.code === "ArrowRight") {
+        playNextClip();
+      } 
+    else if(e.code === "ArrowLeft") { 
+        playPrevClip();
+    }
+    
+
+    function playNextClip() {
+        currentIndex++;
+        if(currentIndex >= Object.keys(clipSet).length) {
+          currentIndex = 0; 
+        }
+        
+        const nextClip = clipSet[Object.keys(clipSet)[currentIndex]];  
+        playClip(nextClip);
+      
+      }
+      
+      function playPrevClip() {
+        currentIndex--;
+        if(currentIndex < 0) {
+          currentIndex = Object.keys(clipSet).length - 1;
+        }
+      
+        const prevClip = clipSet[Object.keys(clipSet)[currentIndex]];
+        playClip(prevClip);
+      
+      }
+      
+      function playClip(clip) {
+
+        if (action) {
+            action.stop();
+        }
+      
+        action = mixer.clipAction(clip);
+        action.reset();  
+        action.play();
+      
+      }
+  
+  });
+    // Play all animations at the same time
+    // clips.forEach(function(clip) {
+    //     const action = mixer.clipAction(clip);
+    //     action.play();
+    // });
+
+}, undefined, function(error) {
+    console.error(error);
+});
+
+const clock = new THREE.Clock();
+function animate() {
+    if(mixer){
+        console.log(clock);
+        mixer.update(clock.getDelta());
+    }
+        
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
